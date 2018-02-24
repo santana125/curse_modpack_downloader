843ab33303c2ae8f90734027e65f81f388577f5a
Gustavo Santana <gustavofelipe68@gmail.com>
2018-02-24 12:54:26 -0300

    Initial commit

---
+++ downloader.py
@@ -0,0 +1,29 @@
+import requests
+from clint.textui import progress
+import lxml.html
+class Downloader():
+    def __init__(self):
+        self.url = ''
+        pass
+    def get_url(self, base_url):
+        if base_url == '':
+           print("Url não encontrada !")
+           return 1
+        url_part = base_url + '/download?client=n'
+        dom = lxml.html.fromstring(requests.get(url_part).content)
+        url_final = [x for x in dom.xpath('//a/@href') if 'file' in x]
+        self.url = 'https://www.curseforge.com' + url_final[0]
+        self.filename = base_url
+        self.filename = self.filename.replace("https://www.curseforge.com/minecraft/modpacks/", "")
+        print(self.filename)
+    def get_file(self):
+        self.temp_file = requests.get(self.url, stream=True)
+        self.path = '/home/gustavo/Projects/'+ self.filename +'.zip'
+        with open(self.path, 'wb') as out:
+            total_length = int(self.temp_file.headers.get('content-length'))
+            for chunk in progress.bar(self.temp_file.iter_content
+                                     (chunk_size=1024),
+                                     expected_size=(total_length/1024) + 1): 
+                if chunk:
+                    out.write(chunk)
+                    out.flush()
---
+++ main.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+#-*- coding: utf-8 -*-
+from downloader import Downloader
+import sys
+
+class Main():
+    def __init__(self):
+        self.url_base = str(sys.argv[1])
+    def run(self):
+        down = Downloader()
+        down.get_url(self.url_base)
+        down.get_file()
+        print(down.url)
+if __name__ == "__main__":
+    app = Main()
+    app.run()
+
