a1b0267eeb46f267038ee94e18a0a4ceff460575
Gustavo Santana <gustavo@Kisum.localhost>
2018-03-20 00:55:52 +0000

    New args system.

--- src/main.py
+++ src/main.py
@@ -1,28 +1,39 @@
 #!/usr/bin/env python3
 #-*- coding: utf-8 -*-
 from downloader import Downloader
-import sys
+import sys, getopt
 
 class Main():
-    def __init__(self):
-        self.args = sys.argv
-        if len(self.args) < 2:
-            print("Faltam argumentos!")
-            exit(0)
-        elif len(self.args) == 2:
-            self.url_base = str(sys.argv[1])
-            self.path = None
-        else: 
-            self.url_base = str(sys.argv[1])
-            self.path = str(sys.argv[2])
+    def __init__(self,argv):
+        self.path = None
+        self.url_base = ""
+
+        try:
+            opts, args = getopt.getopt(argv,"hu:d:", ["help", "url", "dir"])
+
+        except getopt.GetoptError:
+            self.show_help()
+            exit(2)
+
+        for opt, arg in opts:
+            if opt in ("-h", "--help"):
+                self.show_help()
+            elif opt in ("-u", "--url"):
+                self.url_base = arg
+            elif opt in ("-d", "--dir"):
+                self.path = arg
 
     def run(self):
-        print(self.args)
         down = Downloader()
         down.get_url(self.url_base)
         down.get_file(self.path)
-        print(down.url)
+        
+    def show_help(self):
+        print("-h | --help \t\t\t Shows this.")
+        print("-u | --url <Some URL>\t\t Defines the modpack link to download.")
+        print("-d | --dir <Some Directory>\t Defines the directory to save the modpack meta-data.")
+
 if __name__ == "__main__":
-    app = Main()
+    app = Main(sys.argv[1:])
     app.run()
 
